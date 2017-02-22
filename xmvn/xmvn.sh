5630361a6895818faa03d0b00ba8113f80990fe8
gorvin <igor.n.vincic@gmail.com>
2017-02-22 10:57:57 +0100

    Improve xmvn

--- xmvn/xmvn.sh
+++ xmvn/xmvn.sh
@@ -4,7 +4,7 @@
 /usr/bin/Xvfb $DISPLAY -ac -screen 0 1024x768x16 +extension RANDR 2> /dev/null &
 XVFB_PID=$!
 sleep 1
-/usr/bin/mvn $@
+/usr/bin/mvn.real $@
 exit_value=$?
 kill -9 $XVFB_PID 2>&1 1>/dev/null
 #echo DISPLAY=$DISPLAY
