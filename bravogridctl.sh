93bada9d728864a7fb8eb37adb84660c17e3aa33
gorvin <igor.n.vincic@gmail.com>
2017-09-07 10:33:54 +0200

    Fix images names handling.

--- bravogridctl.sh
+++ bravogridctl.sh
@@ -42,19 +42,19 @@ esac
 
 function run_chrome_containers {
      for i in $(seq 0 $(($1-1))); do
-        port=$((6000+i))
+        port=$((16000+i))
         echo starting chrome container with port 5900 mapped to ${port}
         docker run -d -p ${port}:5900 -v /dev/shm:/dev/shm -v /dev/urandom:/dev/random --name ${CONTAINER_PREFIX}chrome${i} -e HUB_PORT_4444_TCP_ADDR=${HUB_IP} -e HUB_PORT_4444_TCP_PORT=4444 \
-         -e NODE_MAX_INSTANCES=1 -e NODE_MAX_SESSION=1 ${PRIVATE_REGISTRY}bravo/grid/chrome${SUFIX}${GRID_VERSION} || exit 1
+         -e NODE_MAX_INSTANCES=1 -e NODE_MAX_SESSION=1 ${PRIVATE_REGISTRY}grid/chrome${GRID_VERSION} || exit 1
     done
 }
 
 function run_firefox_containers {
      for i in $(seq 0 $(($1-1))); do
-        port=$((7000+i))
+        port=$((17000+i))
         echo starting firefox container with port 5900 mapped to ${port}
         docker run -d -p ${port}:5900 -v /dev/urandom:/dev/random --name ${CONTAINER_PREFIX}firefox${i} -e HUB_PORT_4444_TCP_ADDR=${HUB_IP} -e HUB_PORT_4444_TCP_PORT=4444 \
-         -e NODE_MAX_INSTANCES=1 -e NODE_MAX_SESSION=1 ${PRIVATE_REGISTRY}bravo/grid/firefox${SUFIX}${GRID_VERSION} || exit 1
+         -e NODE_MAX_INSTANCES=1 -e NODE_MAX_SESSION=1 ${PRIVATE_REGISTRY}grid/firefox${GRID_VERSION} || exit 1
     done
 }
 
@@ -63,14 +63,11 @@ if [ "$1" == "stop" ]; then
     docker rm -f $(docker ps -a | grep ${CONTAINER_PREFIX}chrome | cut -d ' ' -f 1) 2> /dev/null
     docker rm -f $(docker ps -a | grep ${CONTAINER_PREFIX}hub | cut -d ' ' -f 1) 2> /dev/null
 elif [ "$1" == "start" ]; then
-	if [ ! "$2" == "-no-debug" ]; then
-		SUFIX="-debug"
-	fi
     : ${CHSCALE:=1}
     : ${FFSCALE:=1}
 	GRID_MAX_SESSIONS=$((CHSCALE+FFSCALE))
     echo "CHSCALE=${CHSCALE}, FFSCALE=${FFSCALE}, GRID_MAX_SESSIONS=${GRID_MAX_SESSIONS}"
-    docker run -d -p 4444:4444 -e GRID_MAX_SESSION=${GRID_MAX_SESSIONS} --name ${CONTAINER_PREFIX}hub -h hub ${PRIVATE_REGISTRY}bravo/grid/hub${GRID_VERSION} || exit 1
+    docker run -d -p 4444:4444 -e GRID_MAX_SESSION=${GRID_MAX_SESSIONS} --name ${CONTAINER_PREFIX}hub -h hub ${PRIVATE_REGISTRY}grid/hub${GRID_VERSION} || exit 1
     HUB_IP=$(docker inspect ${CONTAINER_PREFIX}hub | grep \"IPAddress\" | tail -1 | cut -d '"' -f 4)
     : ${GRID_HUB_IP:=${HUB_IP}} # on docker toolbox this is already set few lines earlier
 
